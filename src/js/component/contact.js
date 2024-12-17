9e0eb14e83a3b82c7ed688eedc9b3e61bfd56f6d
P. Pablo Cubells Talavera <pablocub@ucm.es>
2024-12-17 12:45:27 +0000

    contact demo

---
+++ src/js/component/contact.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export const Contact = () => (
+    <section className="container w-50">
+        <div className="d-flex justify-content-center border-bottom py-3 px-3">
+            <img src="https://picsum.photos/id/237/536/354" className="rounded-circle" alt="contact-image.png" style={{width:150,height:150}}/>
+            <div className="ms-5 w-100">
+                <div className="d-flex justify-content-between align-items-center mb-2">
+                    <h5 className="fw-bold text-black m-0">Complete Name</h5>
+                    <div className="d-flex">
+                        <div className="mx-3"><i className="fa-solid fa-pencil"></i></div>
+                        <div className="mx-3"><i className="fa-solid fa-trash-can"></i></div>
+                    </div>
+                </div>
+                <div className="text-black-50 d-flex align-items-center">
+                    <i className="fa-solid fa-location-dot mx-2"></i>
+                    <p className="m-0">5842 Hillcrest Road</p>
+                </div>
+                <div className="text-black-50 d-flex align-items-center">
+                    <i className="fa-solid fa-phone-flip mx-2"></i>
+                    <p className="m-0">(870) 288-4149</p>
+                </div>
+                <div className="text-black-50 d-flex align-items-center">
+                    <i className="fa-solid fa-envelope mx-2"></i>
+                    <p className="m-0">mike.ana@example.com</p>
+                </div>                
+            </div>
+        </div>
+    </section>
+);
